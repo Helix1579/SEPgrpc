f71959824870bc37abedd252b61c87044db39497
Helix1579 <mashruwalayashraj9@gmail.com>
2023-05-21 13:45:05 +0200

    Initial commit

---
+++ src/main/java/com/sep/grpc/SEPgrpcApplication.java
@@ -0,0 +1,13 @@
+package com.sep.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SePgrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SePgrpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sep/grpc/SEPgrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SePgrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
